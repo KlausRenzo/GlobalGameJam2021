120966ddfef98e0a76a1be7b3e0afe44466b5a32
Nicolò Renzoni <renzoni.nicolo@gmail.com>
2021-01-30 17:23:26 +0100

    Terrain Generator

--- Opportunity of Death/Assets/Scripts/TerrainGenerator.cs
+++ Opportunity of Death/Assets/Scripts/TerrainGenerator.cs
@@ -1,13 +1,18 @@
-using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 using Sirenix.OdinInspector;
-using Unity.EditorCoroutines.Editor;
+using Sirenix.Serialization;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [ExecuteInEditMode]
 public class TerrainGenerator : MonoBehaviour
 {
-	[SerializeField] [Range(0, 0.2f)] private float maxHeigth = 0.01f;
-	private Terrain terrain;
+	[SerializeField] [Range(0, 1f)] private float maxHeigth = 0.01f;
+	[SerializeField] private Terrain topLeftTerrain;
+	private List<Terrain> terrains;
 	private int heigthMapWidth;
 	private int heightmapHeight;
 
@@ -17,14 +22,14 @@ public class TerrainGenerator : MonoBehaviour
 	[SerializeField] [MinMaxSlider(-1f, 1f)]
 	private Vector2 heigthCircleDelta;
 
-	[SerializeField] [MinMaxSlider(0,100)]
-	private Vector2 circleNumberRange;
+	[SerializeField] [MinMaxSlider(0, 100)]
+	private Vector2 circleDensity;
 
 	[SerializeField] private AnimationCurve[] craterCurves;
+	[SerializeField] private int x, y;
 
 	private void Awake()
 	{
-		terrain = this.GetComponentInChildren<Terrain>();
 	}
 
 	// Start is called before the first frame update
@@ -37,46 +42,88 @@ public class TerrainGenerator : MonoBehaviour
 	{
 	}
 
+
 	[Button]
 	void Generate()
 	{
-		terrain = this.GetComponentInChildren<Terrain>();
+		terrains = GetComponentsInChildren<Terrain>().ToList();
 
-		int size = terrain.terrainData.heightmapResolution;
+		int minX = (int) (terrains.Min(x => x.transform.position.x) / 1000);
+		int minY = (int) (terrains.Min(x => x.transform.position.z) / 1000);
+		int maxX = (int) (terrains.Max(x => x.transform.position.x) / 1000);
+		int maxY = (int) (terrains.Max(x => x.transform.position.z) / 1000);
 
-		var data = terrain.terrainData;
-		float[,] heights = new float[size, size];
-		WaitForEndOfFrame waitForEndOfFrame = new WaitForEndOfFrame();
-		for (int i = 0; i < size; i++)
+		int hori = (int) (maxX - minX) + 1;
+		int verti = (int) (maxY - minY) + 1;
+
+		var terrainMatrix = new Terrain[hori, verti];
+		int xx = 0;
+		int yy = 0;
+		for (int i = minY; i <= maxY; i++)
 		{
-			for (int j = 0; j < size; j++)
+			for (int j = minX; j <= maxX; j++)
+			{
+				terrainMatrix[xx, yy] = terrains.FirstOrDefault(x => Math.Abs(x.transform.position.x - i* 1000) < 10 && Math.Abs(x.transform.position.z - j * 1000) < 10);
+				xx++;
+			}
+
+			yy++;
+			xx = 0;
+		}
+
+
+		int size = terrains[0].terrainData.heightmapResolution;
+		float[,] heights = new float[size * hori, size * verti];
+
+		for (int i = 0; i < size * hori; i++)
+		{
+			for (int j = 0; j < size * verti; j++)
 			{
 				heights[i, j] = maxHeigth / 2;
 			}
 		}
 
-		data.SetHeights(0, 0, heights);
-
-		int craters = Random.Range(Mathf.RoundToInt(circleNumberRange.x), Mathf.RoundToInt(circleNumberRange.y));
+		int craters = Random.Range(Mathf.RoundToInt(circleDensity.x * Mathf.Sqrt(terrains.Count)), Mathf.RoundToInt(circleDensity.y * Mathf.Sqrt(terrains.Count)));
 		for (int i = 0; i < craters; i++)
 		{
 			var radius = Random.Range(circleDimensions.x, circleDimensions.y);
 			var heigth = Random.Range(heigthCircleDelta.x, heigthCircleDelta.y);
-			var x = Random.Range(0, size);
-			var y = Random.Range(0, size);
-			GenerateMountain(heights, size, new Vector2(x, y), radius, heigth);
+			var x = Random.Range(0, size * hori);
+			var y = Random.Range(0, size * verti);
+			GenerateMountain(heights, size * hori, size * verti, new Vector2(x, y), radius, heigth);
 		}
 
-		data.SetHeights(0, 0, heights);
+
+		for (int i = 0; i < hori; i++)
+		{
+			for (int j = 0; j < verti; j++)
+			{
+				var offsetY = size * i;
+				var offsetX = size * j;
+
+				var h = new float[size, size];
+				for (int a = 0; a < size; a++)
+				{
+					for (int b = 0; b < size; b++)
+					{
+						h[b, a] = heights[offsetY + b, offsetX + a];
+					}
+				}
+
+				terrainMatrix[i, j].terrainData.SetHeights(0, 0, h);
+			}
+		}
 	}
 
-	private void GenerateMountain(float[,] heights, int size, Vector2 center, float radius, float heigth)
+	private void GenerateMountain(float[,] heights, int sizeX, int sizeY, Vector2 center, float radius, float heigth)
 	{
 		var craterCurve = craterCurves[Random.Range(0, craterCurves.Length)];
 		Debug.Log($"Generated circle @({center.x}, {center.y}), radius = {radius}, heigth = {heigth}%");
-		for (int x = 0; x < size; x++)
+
+
+		for (int x = Mathf.Max(0, Mathf.FloorToInt(center.x - radius)); x < Mathf.Min(sizeX,Mathf.CeilToInt(center.x + radius)); x++)
 		{
-			for (int y = 0; y < size; y++)
+			for (int y = Mathf.Max(0, Mathf.FloorToInt(center.y - radius)); y < Mathf.Min(sizeY, Mathf.CeilToInt(center.y + radius)); y++)
 			{
 				var res = Mathf.Pow(x - center.x, 2) + Mathf.Pow(y - center.y, 2);
 
@@ -91,21 +138,21 @@ public class TerrainGenerator : MonoBehaviour
 	[Button]
 	private void Smooth()
 	{
-		terrain = this.GetComponentInChildren<Terrain>();
+		//terrain = this.GetComponentInChildren<Terrain>();
 
-		int size = terrain.terrainData.heightmapResolution;
-		var data = terrain.terrainData;
-		float[,] heights = data.GetHeights(0, 0, size, size);
+		//int size = terrain.terrainData.heightmapResolution;
+		//var data = terrain.terrainData;
+		//float[,] heights = data.GetHeights(0, 0, size, size);
 
-		for (int i = 1; i < size - 1; i++)
-		{
-			for (int j = 1; j < size - 1; j++)
-			{
-				heights[i, j] = (heights[i - 1, j - 1] + heights[i + 1, j + 1] + heights[i - 1, j + 1] + heights[i + 1, j - 1]) / 4;
-			}
-		}
+		//for (int i = 1; i < size - 1; i++)
+		//{
+		//	for (int j = 1; j < size - 1; j++)
+		//	{
+		//		heights[i, j] = (heights[i - 1, j - 1] + heights[i + 1, j + 1] + heights[i - 1, j + 1] + heights[i + 1, j - 1]) / 4;
+		//	}
+		//}
 
 
-		data.SetHeights(0, 0, heights);
+		//data.SetHeights(0, 0, heights);
 	}
 }
