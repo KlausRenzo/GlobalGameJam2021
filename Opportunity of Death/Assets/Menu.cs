fbcfda3f900216b3be6ae699ed91870b3b818d97
Nicolò Renzoni <renzoni.nicolo@gmail.com>
2021-01-31 14:49:13 +0100

    Added start menu

---
+++ Opportunity of Death/Assets/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+	public void StartButton()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void CreditsButton()
+	{
+
+	}
+
+	public void QuitButton()
+	{
+	}
+}
